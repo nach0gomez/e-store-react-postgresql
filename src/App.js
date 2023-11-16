46703e896737b18a40889c9d2c1a482f4bfd7485
nach0gomez <terminatitorxdyt@gmail.com>
2023-11-15 20:51:46 -0500

    Add API request to fetch and display data in App component.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  // create an array to store the data from the json request
+  const [resultados, setResultados] = useState([])
+
+  console.log('Renderizando App'); // Agrega esta línea
+
+  // we make sure it is called once the page is mounted 
+  React.useEffect(() => {
+    fetch('http://localhost:3001/categorias')
+      .then(response => response.json())
+      .then(data => {
+        console.log(data);
+        setResultados(data);
+      })
+  }, [])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {
+        resultados.map( d => (
+          <div key={d.id}> {d.title} </div>
+        ))
+      }
     </div>
   );
 }
