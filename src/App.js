ec3486a5f07f8552c4047e577afa27804760adfb
nach0gomez <terminatitorxdyt@gmail.com>
2023-11-15 22:08:22 -0500

    Add useEffect hook to fetch data from API and update state

--- src/App.js
+++ src/App.js
@@ -4,19 +4,24 @@ import './App.css';
 
 function App() {
   // create an array to store the data from the json request
-  const [resultados, setResultados] = useState([])
+  const [resultados, setResultados] = useState([]); 
 
   console.log('Renderizando App'); // Agrega esta línea
 
   // we make sure it is called once the page is mounted 
   React.useEffect(() => {
+    //console.log("Ejecuntando useEffect")
     fetch('http://localhost:3001/categorias')
       .then(response => response.json())
       .then(data => {
         console.log(data);
+        //console.log("Datos recibidos")
         setResultados(data);
-      })
+      });
   }, [])
+
+  
+
 
   return (
     <div className="App">
@@ -24,7 +29,7 @@ function App() {
         resultados.map( d => (
           <div key={d.id}> {d.title} </div>
         ))
-      }
+      }      
     </div>
   );
 }
