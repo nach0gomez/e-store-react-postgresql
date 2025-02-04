33afa7063d8890f02742cefb564e6a19f085cf54
Nacho Gómez <terminatitorxdyt@gmail.com>
2025-02-04 16:47:14 -0500

    feat(Layout, styles): Implement collapsible sidebar and update styles; change font to Montserrat

--- front-end/src/components/layout.js
+++ front-end/src/components/layout.js
@@ -1,37 +1,42 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link, Outlet } from 'react-router-dom'
 import '../styles/layout.css'
 import { HomeIcon, ShoppingCartIcon } from './icons'
 import Search from './search'
 
 const Layout = ({ categories }) => {
-  // render the categories, this is done when the page loads for first time
-  const renderCategories = () => {
-    return categories.map(c =>
-      <li className='categories' key={c.id_category}> <Link to={`/categories/${c.id}`}> {c.title}  </Link> </li>
-    )
-  }
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false)
 
   return (
-    <>
+    <aside>
       <header>
         <div id='headerHomeIcon'>
           <Link to='/'><HomeIcon width={40} /></Link>
         </div>
-
         <Search />
-
         <div id='headerTitle'>Shopper</div>
         <div id='headerCartIcon'>
-          <Link to='/cart'><ShoppingCartIcon width={40} /> </Link>
+          <Link to='/cart'><ShoppingCartIcon width={40} /></Link>
         </div>
       </header>
 
+      {/* Botón para abrir/cerrar la barra lateral */}
+      <button className='toggle-sidebar' onClick={() => setIsSidebarOpen(!isSidebarOpen)}>
+        ☰
+      </button>
+
       <section>
-        <nav>
-          Categories
+        {/* Sidebar colapsable */}
+        <nav className={`sidebar ${isSidebarOpen ? 'open' : 'closed'}`}>
+          <h2>Categories</h2>
           <ul>
-            {categories && renderCategories()}
+            {categories.map((c) => (
+              <li key={c.id_category} className='categories'>
+                <Link to={`/categories/${c.id}`}>
+                  {c.title}
+                </Link>
+              </li>
+            ))}
           </ul>
         </nav>
 
@@ -39,12 +44,13 @@ const Layout = ({ categories }) => {
           <Outlet />
         </main>
       </section>
+
       <footer>
         <Link to='/'>Home</Link> | <Link to='/cart'>Cart</Link>
         <br />
         <p>All rights reserved</p>
       </footer>
-    </>
+    </aside>
   )
 }
 
